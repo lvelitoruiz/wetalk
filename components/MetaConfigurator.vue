510f2102d2ffdbde52019be931af5f8610c08aa9
Luis Velito <luisvelito@Luiss-Mac-Studio.local>
2023-10-11 15:22:16 -0500

    feat(metas): css changes corrections

--- components/MetaConfigurator.vue
+++ components/MetaConfigurator.vue
@@ -2,24 +2,29 @@
 import { ref, watchEffect } from "vue";
 import { apiKey, apiUrl } from "~/consts";
 import { useMetaStore } from "../stores/meta";
+import GraphemeSplitter from "grapheme-splitter";
 
 const metaStore = useMetaStore();
 const metaData = ref(null);
 const illustrations = ref(null);
 const meta = ref("");
 const selectedColor = ref("#FE91A4");
-const selectedImage = ref("https://wetalk-directus-dev-upc.stage01.link/assets/b2155346-5f91-4749-91cb-77c50355c1e0");
-
+const selectedImage = ref(
+  "https://wetalk-directus-dev-upc.stage01.link/assets/b2155346-5f91-4749-91cb-77c50355c1e0"
+);
 
 const handleMeta = (event) => {
-  if (meta.value.length > 65) {
-    meta.value = meta.value.slice(0, 65);
+  const splitter = new GraphemeSplitter();
+  const graphemes = splitter.splitGraphemes(meta.value);
+
+  if (graphemes.length > 65) {
+    meta.value = graphemes.slice(0, 65).join("");
   }
 };
 
 const fetchMetaInfo = async () => {
-  await metaStore.fetchMetaData(apiUrl, apiKey, "U2020201234178"); 
-  await metaStore.obtainImages(apiUrl)
+  await metaStore.fetchMetaData(apiUrl, apiKey, "U2020201234178");
+  await metaStore.obtainImages(apiUrl);
 };
 
 watchEffect(() => {
@@ -31,39 +36,27 @@ watchEffect(() => {
     selectedColor.value = metaData.value?.color;
     selectedImage.value = metaData.value?.imagen;
   }
-  if(images) {
-    const formattedImages = images.map(item => ({
-        imagen: item.imagen,
-        categoria: item.categoria,
-        active: false,
-      }))
+  if (images) {
+    const formattedImages = images.map((item) => ({
+      imagen: item.imagen,
+      categoria: item.categoria,
+      active: false,
+    }));
 
-    console.log(formattedImages);
     illustrations.value = formattedImages;
   }
 });
 
 onMounted(async () => {
-  fetchMetaInfo();
+  await fetchMetaInfo();
+  illustrations.value.map( item => {
+    if(item.imagen === selectedImage.value) {
+      item.active = true;
+    } else {
+      item.active = false;
+    }
+  })
 });
-
-// const illustrations = [
-//   {
-//     name: "estudios.svg",
-//     image: "_nuxt/assets/images/estudios.svg",
-//     active: true,
-//   },
-//   {
-//     name: "mundo.svg",
-//     image: "_nuxt/assets/images/mundo.svg",
-//     active: false,
-//   },
-//   {
-//     name: "posgrado.svg",
-//     image: "_nuxt/assets/images/posgrado.svg",
-//     active: false,
-//   },
-// ];
 
 const hasMeta = computed(() => meta.value !== "");
 
@@ -90,6 +83,7 @@ const cleanMeta = async () => {
 };
 
 const changeImage = (indexItem, newImage) => {
+  console.log('calling illustrations',newImage," ",indexItem);
   illustrations.value.map((item, index) => {
     if (index === indexItem) {
       item.active = true;
@@ -102,7 +96,7 @@ const changeImage = (indexItem, newImage) => {
 </script>
 <template>
   <ContainerBoxSimple>
-    <div class="flex items-center justify-between pb-[24px]">
+    <div class="flex items-center justify-between pb-6">
       <h3 class="text-[#404040] text-[32px] font-bold font-solano uppercase">
         Mi meta
       </h3>
@@ -122,15 +116,17 @@ const changeImage = (indexItem, newImage) => {
       </div>
     </div>
     <div class="flex gap-[20px]">
-      <div class="min-w-[340px] max-w-[340px] border-r border-[#D9D9D9] pr-[28px]">
+      <div
+        class="min-w-[369px] max-w-[369px] border-r border-[#D9D9D9] pr-7"
+      >
         <div class="flex items-center justify-center flex-col gap-[14px]">
-          <img class="min-w-[224px] h-[175px]" :src="selectedImage" alt="" />
+          <img class="min-w-[255px] h-auto" :src="selectedImage" alt="" />
           <div class="relative flex items-center flex-col justify-center">
             <div class="triangulo"></div>
             <div
               class="bg-white shadow-[0_0_20px_0_rgba(77,39,37,0.25)] px-5 py-2 w-full flex justify-between items-center rounded-lg"
             >
-              <p class="text-sm text-[#A6A6A6] min-h-[16px] min-w-[40px]">
+              <p class="text-sm text-center text-[#A6A6A6] min-h-[16px] min-w-[40px]">
                 {{ meta }}
               </p>
             </div>
@@ -144,10 +140,10 @@ const changeImage = (indexItem, newImage) => {
               Selecciona una ilustración
             </p>
           </div>
-          <div class="flex gap-2">
+          <div class="grid grid-cols-3 gap-4">
             <img
               v-for="(illustration, index) in illustrations"
-              class="min-w-[100px] h-[78px] cursor-pointer rounded-md"
+              class="min-w-[100px] min-h-[78px] col-span-1 h-auto cursor-pointer rounded-md"
               :class="{
                 'border-2 border-gray-700': illustration.active,
                 'border border-gray-400': !illustration.active,
@@ -163,7 +159,7 @@ const changeImage = (indexItem, newImage) => {
             <p class="text-[#404040] font-bold font-solano uppercase">
               Cuéntanos tu meta
             </p>
-            <p class="text-xs text-[#808080]">Máximo de caracteres : 65</p>
+            <p class="text-xs text-[#808080]">{{ meta.length || 0 }}/65</p>
           </div>
           <input
             v-model="meta"
@@ -173,43 +169,6 @@ const changeImage = (indexItem, newImage) => {
             @input="handleMeta"
           />
         </div>
-        <!-- <div>
-          <div class="flex items-center justify-between pb-2">
-            <p class="text-[#404040] font-bold font-solano uppercase">Color</p>
-          </div>
-          <div class="flex gap-2 items-center">
-            <div class="cursor-pointer">
-              <div
-                class="bg-[#6644FF] rounded-full h-[25px] w-[25px]"
-                @click="() => changeColor('#6644FF')"
-              ></div>
-            </div>
-            <div class="cursor-pointer">
-              <div
-                class="bg-[#3399FF] rounded-full h-[25px] w-[25px]"
-                @click="() => changeColor('#3399FF')"
-              ></div>
-            </div>
-            <div class="cursor-pointer">
-              <div
-                class="bg-[#FFA439] rounded-full h-[25px] w-[25px]"
-                @click="() => changeColor('#FFA439')"
-              ></div>
-            </div>
-            <div class="cursor-pointer">
-              <div
-                class="bg-[#E35169] rounded-full h-[25px] w-[25px]"
-                @click="() => changeColor('#E35169')"
-              ></div>
-            </div>
-            <div class="cursor-pointer">
-              <div
-                class="bg-[#2ECDA7] rounded-full h-[25px] w-[25px]"
-                @click="() => changeColor('#2ECDA7')"
-              ></div>
-            </div>
-          </div>
-        </div> -->
       </div>
     </div>
   </ContainerBoxSimple>
