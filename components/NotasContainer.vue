344ca9e5060112c4af7f3a8465732e3ef6aed86f
lvelitoruiz <pogolvelito@gmail.com>
2023-11-13 15:33:51 -0500

    notas

---
+++ components/NotasContainer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-[1023px] mx-auto">
+        <div class="flex gap-[28px]">
+          <div class="min-w-[65%]">
+            <NoteBox />
+          </div>
+          <div class="flex-col flex gap-7"> 
+            <InasistenciasInfo inasistencia="2" :dates="datesInasistencia" />
+            <TeacherBox />
+            <HelpBox />
+          </div>
+        </div>
+      </div>
+</template>
+
+<script setup>
+
+const datesInasistencia = [
+    '03/10/2023',
+    '05/10/2023'
+]
+
+</script>
+
+<style>
+
+</style>
---
+++ components/NoteBox.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="pb-7">
+      <BoxContainer color="green">
+        <div class="flex items-center justify-between pb-3 px-[9px]">
+          <h3 class="text-[#404040] text-2xl font-bold font-solano uppercase">
+            Notas
+          </h3>
+          <router-link class="flex items-center gap-2" to="/notas">
+            <span class="text-[#E50A17] font-bold font-zizou-bold text-sm"
+              >Ver más</span
+            >
+            <i class="icon-arrow-right text-[#E50A17]"></i>
+          </router-link>
+          
+        </div>
+        <div class="flex items-center justify-start mb-3">
+          <div v-if="inasistencia"
+            class="border border-[#404040] px-4 gap-1 flex items-center justify-start h-[28px] rounded-full"
+          >
+            <p class="text-[#404040] font-sans text-sm">Inasistencias:</p>
+            <p class="text-[#404040] font-sans text-sm">{{ inasistencia }}</p>
+          </div>
+        </div>
+        <NotesTable :notasData="tablaNotas" />
+      </BoxContainer>
+    </div>
+  </div>
+</template>
+
+<script setup >
+
+const props = defineProps ({
+  inasistencia : Number
+})
+
+const tablaNotas = [
+  {
+    tipo: "PA1",
+    evaluacion: "Prod. oral - sesión sinc.",
+    peso: "20%",
+    nota: 20,
+  },
+  {
+    tipo: "PA1",
+    evaluacion: "Prod. oral - sesión sinc.",
+    peso: "20%",
+    nota: 20,
+  },
+  {
+    tipo: "PA1",
+    evaluacion: "Prod. oral - sesión sinc.",
+    peso: "20%",
+    nota: 20,
+  },
+];
+</script>
