484dd5e106c6bd5a5822579710a9139912daa007
Luis Velito <luisvelito@Luiss-Mac-Studio.local>
2023-11-14 11:40:15 -0500

    courses today

--- components/NoteBox.vue
+++ components/NoteBox.vue
@@ -6,39 +6,30 @@
           <h3 class="text-[#404040] text-2xl font-bold font-solano uppercase">
             Notas
           </h3>
-          <router-link
-            v-if="dashboard"
-            class="flex items-center gap-2"
-            to="/notas"
-          >
-            <span class="text-[#E50A17] font-bold font-zizou-bold text-sm"
-              >Ver más</span
-            >
+          <router-link v-if="dashboard" class="flex items-center gap-2" to="/notas">
+            <span class="text-[#E50A17] font-bold font-zizou-bold text-sm">Ver más</span>
             <i class="icon-arrow-right text-[#E50A17]"></i>
           </router-link>
           <div v-else>
-            <div v-if="promedio">
-              <Button
-              @click="irSimulador"
-              label="Simulador de promedio"
-              primary
-            />
+            <div v-if="!showSimulator">
+              <Button @click="irSimulador" label="Simulador de promedio" primary />
             </div>
             <div v-else class="flex gap-4">
-              <Button @click="promedio" label="Cancelar" secundary />
-              <Button label="Reestablecer notas" primary />
+              <Button @click="irSimulador" label="Cancelar" secundary />
+              <Button @click="irSimulador" label="Reestablecer notas" primary />
             </div>
           </div>
         </div>
         <div class="flex items-center justify-start mb-3">
-          <div v-if="inasistencia !== null"
-            class="border border-[#404040] px-4 gap-1 flex items-center justify-start h-[28px] rounded-full"
-          >
-            <p class="text-[#404040] font-sans text-sm">Inasistencias:</p>
-            <p class="text-[#404040] font-sans text-sm">{{ inasistencia }}</p>
+          <div v-if="dashboard">
+            <div v-if="inasistencia !== null"
+              class="border border-[#404040] px-4 gap-1 flex items-center justify-start h-[28px] rounded-full">
+              <p class="text-[#404040] font-sans text-sm">Inasistencias:</p>
+              <p class="text-[#404040] font-sans text-sm">{{ inasistencia }}</p>
+            </div>
           </div>
         </div>
-        <NotesTable :notasData="notasData" :dashboard="dashboard" />
+        <NotesTable :notasData="notasData" :dashboard="dashboard" :simulator="showSimulator" />
       </BoxContainer>
     </div>
   </div>
@@ -49,20 +40,26 @@ import { useMenuStore } from "../stores/menu";
 const notasData = ref(null);
 const inasistencia = ref(null);
 
+const showSimulator = ref(false);
+
 const menuStore = useMenuStore();
 
-const props = defineProps ({
+const props = defineProps({
   dashboard: Boolean
 })
 
 watchEffect(async () => {
-    const notas = menuStore.getNotasItems;
-    console.log('getting information here: ',notas.notas);
-    if(notas) {
-      notasData.value = notas.notas;
-      inasistencia.value = notas.inasistencias;
-    }
+  const notas = menuStore.getNotasItems;
+  console.log('getting information here: ', notas.notas);
+  if (notas) {
+    notasData.value = notas.notas;
+    inasistencia.value = notas.inasistencias;
+  }
 });
+
+const irSimulador = () => {
+  showSimulator.value = !showSimulator.value;
+}
 
 
 </script>
--- components/NotesTable.vue
+++ components/NotesTable.vue
@@ -37,12 +37,12 @@
               </span>
             </div>
             <div v-else>
-              <span v-if="promedio"
+              <span v-if="!simulator"
                 class="font-sans w-[32px] h-[26px] text-sm font-bold rounded-[6px] flex items-center text-white justify-center"
                 :class="getColorClass(data.nota)">
                 {{ data.nota }}
               </span>
-              <input v-else type="text" v-model="data.nota"
+              <input v-else type="text" v-model="calculus[index].nota"
                 class="w-[32px] border rounded h-[26px] text-sm font-normal font-sans flex items-center text-[404040] justify-center text-center" />
             </div>
           </td>
@@ -52,20 +52,28 @@
   </div>
   <div class="flex items-center justify-between bg-[#DEF7F1] px-[9px] py-[10px] rounded-[10px]">
     <p class="text-[#404040] text-sm font-bold">Promedio final</p>
-    <span class="w-[32px] h-[26px] text-sm font-bold rounded-[6px] flex items-center text-white justify-center"
+    <span v-if="!simulator" class="w-[32px] h-[26px] text-sm font-bold rounded-[6px] flex items-center text-white justify-center"
       :class="getColorClass(promedio[0].nota)">{{ promedio[0].nota }}</span>
+    <span v-else class="w-[32px] h-[26px] text-sm font-bold rounded-[6px] flex items-center text-white justify-center"
+      :class="getColorClass(newVariable)">{{ newVariable }}</span>
   </div>
 </template>
 
 <script setup>
 const notas = ref(null);
+const calculus = ref(null);
 const promedio = ref(null);
+const newVariable = ref(null);
 const props = defineProps({
   notasData: Array,
   dashboard: Boolean,
+  simulator: Boolean,
 });
 
+const newvalue = JSON.parse(JSON.stringify(props.notasData.slice(0, -2)));
+
 notas.value = props.notasData.slice(0, -2);
+calculus.value = [...newvalue];
 promedio.value = props.notasData.slice(-2, -1);
 console.log(promedio.value);
 
@@ -78,4 +86,19 @@ const getColorClass = (nota) => {
     return 'bg-[#039C6D]';
   }
 };
+
+const recalculateNewVariable = () => {
+  const inputs = calculus.value.map((data) => data.nota);
+
+  // Assuming inputs are numbers, you might need to parse them if they are strings
+  const weightedSum = 0.15 * inputs[0] + 0.15 * inputs[1] + 0.15 * inputs[2] + 0.15 * inputs[3] + 0.4 * inputs[4];
+
+  console.log(weightedSum);
+
+  // Update the new variable
+  newVariable.value = weightedSum.toFixed();
+};
+
+// Watch for changes in the notas array and recalculate the new variable
+watch(calculus, recalculateNewVariable, { deep: true });
 </script>
