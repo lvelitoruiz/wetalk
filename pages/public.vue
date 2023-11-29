0bea33b0d47871567cc2ec82561d20b2ab4b0dc7
lvelitoruiz <pogolvelito@gmail.com>
2023-11-29 10:03:02 -0500

    text

--- pages/public.vue
+++ pages/public.vue
@@ -8,7 +8,7 @@
           <img class="w-full md:w-fit md:h-[271px]" src="@/assets/images/matricula.png" alt="">
           <div class="flex flex-col mt-6 lg:mt-0 gap-4 text-center items-center lg:text-end lg:items-end">
             <h3 class="uppercase text-[32px] font-solano font-bold">¿Aún no estás matriculado?</h3>
-            <p class="max-w-[492px] font-publicSans">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+            <p class="max-w-[492px] font-publicSans">Únete a más de 0000 egresados que abrieron las puertas a su próxima beca, maestría o trabajo en el extranjero.</p>
             <Button label="Más información" secundary />
           </div>
         </div>
