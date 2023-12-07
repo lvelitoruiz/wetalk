cc6d808a9ad7d2d9763b5f6f583a0abd803e9049
Luis Velito <luisvelito@Luiss-Mac-Studio.local>
2023-12-07 08:00:01 -0500

    emergency changes

--- pages/index.vue
+++ pages/index.vue
@@ -160,12 +160,14 @@ watchEffect(async () => {
       </div>
       <div class="relative min-w-[20px] text-center py-10 min-h-[78px] box-content">
         <Transition>
-          <div v-if="metaData !== null && metaData[0]?.meta !== ''">
-          <!-- <div v-if="metaData !== null && metaData[0]?.meta !== ''"> -->
-            <p class="text-[#344D47] lg:text-[24px] text-base">Tu meta:</p>
-            <p class="text-[#344D47] lg:text-[28px] text-[18px] uppercase font-bold font-solano">
-              {{ metaData[0]?.meta }}
-            </p>
+          <div v-if="metaData !== null">
+            <div v-if="metaData.length > 0 && metaData[0]?.meta !== ''">
+            <!-- <div v-if="metaData !== null && metaData[0]?.meta !== ''"> -->
+              <p class="text-[#344D47] lg:text-[24px] text-base">Tu meta:</p>
+              <p class="text-[#344D47] lg:text-[28px] text-[18px] uppercase font-bold font-solano">
+                {{ metaData[0]?.meta }}
+              </p>
+            </div>
           </div>
         </Transition>
         <div else>
