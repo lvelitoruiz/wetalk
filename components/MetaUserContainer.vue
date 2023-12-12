fc2c3f7b1276bc7a766f510cf927f68163ed5534
Magno <magpex13@github.com>
2023-12-11 20:10:30 -0500

    -dev hard

--- components/MetaUserContainer.vue
+++ components/MetaUserContainer.vue
@@ -1,23 +1,3 @@
-<script setup>
-import { useMetaStore } from "../stores/meta";
-const selectedImage = ref(
-  "https://adminmi-cert-wetalk.stage01.link/assets/7b4478d8-fbed-4f6f-a000-6868f7b3f400"
-);
-const metaText = ref("");
-
-const metaStore = useMetaStore();
-
-watchEffect(() => {
-  const data = metaStore.getMetaData;
-  if (data.length > 0) {
-    selectedImage.value = data[0].imagen;
-    metaText.value = data[0].meta;
-  }
-});
-
-const isOpen = true;
-const haveLink = true;
-</script>
 <template>
   <Breadcrumb page="Mi perfil" />
   
