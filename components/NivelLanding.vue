e790cad6fe5cbbf7ba8d12cbf545b4c241784e2b
CAMPOS DE LA CRUZ <betsy.campos@laureate.pe>
2023-12-12 08:27:58 -0500

    Ajustes de banner y niveles wetalk

--- components/NivelLanding.vue
+++ components/NivelLanding.vue
@@ -12,7 +12,7 @@
                 <p class="text-xl uppercase font-solano font-bold">English</p>
                 <p class="text-[40px] font-solano lg:text-5xl uppercase font-bold leading-none">5</p>
               </div>
-              <div class="flex justify-center flex-col bg-[#0065FF] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
+              <div class="flex justify-center flex-col bg-[#0043AA] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
                 <p class="font-publicSans">Hablar en inglés con flexibilidad con fines sociales y académicos. Además, escribir sobre temas complejos.</p>
               </div>
             </div>
@@ -21,35 +21,35 @@
                 <p class="text-xl font-solano uppercase font-bold">English</p>
                 <p class="text-[40px] font-solano lg:text-5xl uppercase font-bold leading-none">4</p>
               </div>
-              <div class="flex justify-center flex-col bg-[#0065FF] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-[98%]">
-                <p class="font-publicSans">Interactuar de manera espontánea con personas nativas y escribir con claridad y detalles.</p>
+              <div class="flex justify-center flex-col bg-[#0043AA] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-[98%]">
+                <p class="font-publicSans">Comprender ideas principales en textos complejos dentro de su especialización. Puede comunicarse con hablantes nativos con fluidez, producir textos detallados sobre diversos temas y defender puntos de vista, analizando pros y contras en temas generales.</p>
               </div>
             </div>
             <div class="rounded-md flex lg:items-center lg:ml-[40px]">
               <div class="flex items-center justify-center flex-col bg-[#FDBA30] min-w-[90px] min-h-[112px] lg:min-h-[90px] text-[#191919] rounded-l-md">
                 <p class="text-xl uppercase font-solano font-bold">English</p>
-                <p class="text-[40px] lg:text-5xl font-solano uppercase font-bold leading-none">3-2</p>
+                <p class="text-[40px] lg:text-5xl font-solano uppercase font-bold leading-none">3</p>
               </div>
-              <div class="flex justify-center flex-col bg-[#0065FF] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
-                <p class="font-publicSans">Manejarte durante un viaje y escribir textos sencillos de temas conocidos</p>
+              <div class="flex justify-center flex-col bg-[#0043AA] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
+                <p class="font-publicSans">Entender los puntos clave en textos estándar sobre temas familiares en diferentes contextos. Desenvolverse en situaciones comunes durante viajes y producir textos coherentes sobre temas familiares. Describir experiencias, eventos, deseos y justificar sus opiniones brevemente.</p>
               </div>
             </div>
             <div class="rounded-md flex lg:items-center lg:ml-[60px]">
               <div class="flex items-center justify-center flex-col bg-[#FDBA30] min-w-[90px] min-h-[112px] lg:min-h-[90px] text-[#191919] rounded-l-md">
                 <p class="text-xl uppercase font-solano font-bold">English</p>
-                <p class="text-[40px] lg:text-5xl font-solano uppercase font-bold leading-none">1</p>
+                <p class="text-[40px] lg:text-5xl font-solano uppercase font-bold leading-none">2</p>
               </div>
-              <div class="flex justify-center flex-col bg-[#0065FF] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
-                <p class="font-publicSans">Comunicarte en tareas simples y rutinarias.</p>
+              <div class="flex justify-center flex-col bg-[#0043AA] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
+                <p class="font-publicSans">Comprender frases relacionadas con áreas importantes de su vida diaria. Comunicarse en tareas simples y cotidianas, describir aspectos básicos de su pasado y entorno, y expresar necesidades inmediatas con intercambios directos.</p>
               </div>
             </div>
             <div class="rounded-md flex lg:items-center lg:ml-[80px]">
               <div class="flex items-center justify-center flex-col bg-[#FDBA30] min-w-[90px] min-h-[112px] lg:min-h-[90px] text-[#191919] rounded-l-md">
                 <p class="text-xl uppercase font-solano font-bold">English</p>
-                <p class="text-[40px] font-solano lg:text-5xl uppercase font-bold leading-none">0</p>
+                <p class="text-[40px] font-solano lg:text-5xl uppercase font-bold leading-none">0-1</p>
               </div>
-              <div class="flex justify-center flex-col bg-[#0065FF] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
-                <p class="font-publicSans">Interactuar de manera sencilla con un interlocutor paciente.</p>
+              <div class="flex justify-center flex-col bg-[#0043AA] lg:h-[90px] text-white rounded-r-md px-4 py-4 lg:py-0 w-full">
+                <p class="font-publicSans">Entender y usar expresiones cotidianas y frases simples para satisfacer necesidades inmediatas. Presentarse, proporcionar información personal básica y relacionarse de manera elemental en situaciones donde la comunicación sea clara y lenta.</p>
               </div>
             </div>
           </div>
