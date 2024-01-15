4f0b4d85dd74f1fa06b3aa7104a971f58bb048b0
CAMPOS DE LA CRUZ <betsy.campos@laureate.pe>
2024-01-15 09:27:46 -0500

    Slider home

---
+++ components/BeyondCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  dataPost: Object,
+});
+
+const dataList = props.dataPost.sort((a, b) => a.destacado - b.destacado).slice(0, 6);
+
+
+</script>
+<template>
+    <GenericCarouselSlider :dataList="dataList">
+        <template v-slot="slotProps">
+            <ScheduleCardBeyond :item="slotProps.item"/>
+        </template>
+    </GenericCarouselSlider>
+</template>
