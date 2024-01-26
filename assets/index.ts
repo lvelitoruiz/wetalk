f6fcb14cf4726935bf2cba723c4bfa3c2b51eb16
Magno <magpex13@github.com>
2024-01-26 17:22:53 -0500

    Fixes 3

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,4 @@
+export { default as spinner } from '@/assets/images/spinner.gif';
 export { default as arrow_right_gray } from '@/assets/images/arrow_right_gray.svg';
 export { default as arrow_bottom_black } from '@/assets/images/arrow_bottom_black.svg';
 export { default as icon_close_black } from '@/assets/images/icon_close_black.svg';
--- components/schedules/status/StatusLoading.vue
+++ components/schedules/status/StatusLoading.vue
@@ -1,5 +1,8 @@
+<script>
+import { spinner } from '../../../assets/index.ts';
+</script>
 <template>
 	<div class="flex items-center justify-center">
-		<img class="w-[45px]" src="../../../assets/images/spinner.gif" />
+		<img class="w-[45px]" :src="spinner" />
 	</div>
 </template>
